33a0e28c96308f7e90881856cc89ee4b94b2d489
Vincent Grossier <vincent.grossier@gmail.com>
2024-08-01 17:04:31 -0400

    Initial Commit

---
+++ BitcoinExplorer/BitcoinExplorerApp.swift
@@ -0,0 +1,17 @@
+//
+//  BitcoinExplorerApp.swift
+//  BitcoinExplorer
+//
+//  Created by Vincent Grossier on 2024-08-01.
+//
+
+import SwiftUI
+
+@main
+struct BitcoinExplorerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BitcoinExplorer/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  BitcoinExplorer
+//
+//  Created by Vincent Grossier on 2024-08-01.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
